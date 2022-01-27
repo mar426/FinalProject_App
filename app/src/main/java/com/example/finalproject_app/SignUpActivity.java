df6ba09286a1957ae0348be409ce888dbf875bcf
mar426 <78439770+mar426@users.noreply.github.com>
2022-01-27 20:45:25 +0200

    sign up POST

--- app/src/main/java/com/example/finalproject_app/SignUpActivity.java
+++ app/src/main/java/com/example/finalproject_app/SignUpActivity.java
@@ -4,10 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.example.finalproject_app.HTTP.HttpCall;
+import com.example.finalproject_app.HTTP.HttpRequest;
+
+import java.util.HashMap;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -21,12 +27,15 @@ public class SignUpActivity extends AppCompatActivity {
     private EditText signup_verify_password;
     //Buttons
     private Button signup_btn;
+    //http
+    private static final String ADD_USER_URL = "http://10.0.2.2:3000/users/addUser";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
         signup_btn=findViewById(R.id.signup_btn);
+        signup_ID=findViewById(R.id.signup_UserID);
         signup_fullName=findViewById(R.id.signup_fullName);
         signup_email=findViewById(R.id.signup_email);
         signup_age=findViewById(R.id.signup_age);
@@ -39,7 +48,30 @@ public class SignUpActivity extends AppCompatActivity {
         signup_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                HttpCall httpCallPost = new HttpCall();
+                httpCallPost.setMethodtype(HttpCall.POST);
+                httpCallPost.setUrl(ADD_USER_URL);
+                HashMap<String,String> paramsPost = new HashMap<>();
+                Editable userID = signup_ID.getText();
+                Editable fullName = signup_fullName.getText();
+                Editable email = signup_email.getText();
+                Editable age = signup_age.getText();
+                Editable height = signup_height.getText();
+                Editable password = signup_password.getText();
+                paramsPost.put("userID", String.valueOf(userID));
+                paramsPost.put("fullName", String.valueOf(fullName));
+                paramsPost.put("email", String.valueOf(email));
+                paramsPost.put("age", String.valueOf(age));
+                paramsPost.put("height", String.valueOf(height));
+                paramsPost.put("password", String.valueOf(password));
+                httpCallPost.setParams(paramsPost);
+                new HttpRequest(){
+                    @Override
+                    public void onResponse(String response) {
+                        super.onResponse(response);
+                        //PostServerResponse.setText("Post:" +response);
+                    }
+                }.execute(httpCallPost);
                 startActivity(new Intent(SignUpActivity.this,LoginActivity.class));
             }
         });
