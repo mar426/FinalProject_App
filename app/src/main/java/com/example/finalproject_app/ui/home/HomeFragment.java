0903f9404b3f16189142005c0e3f315586d2bd6e
mar426 <78439770+mar426@users.noreply.github.com>
2022-02-01 20:04:10 +0200

    selected attractions update

--- app/src/main/java/com/example/finalproject_app/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/finalproject_app/ui/home/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.finalproject_app.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -8,22 +9,29 @@ import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.finalproject_app.HTTP.HttpCall;
+import com.example.finalproject_app.HTTP.HttpRequest;
+import com.example.finalproject_app.MainActivity;
 import com.example.finalproject_app.Model.Atraction_Adapter;
 import com.example.finalproject_app.Model.Attraction;
 import com.example.finalproject_app.R;
+import com.example.finalproject_app.SignUpActivity;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class HomeFragment extends Fragment {
 
     private RecyclerView recyclerView;
     Button route_btn;
+    private static final String UPDATE_SA_USER_URL = "http://10.0.2.2:3000/users/selected_attraction_update";
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -51,7 +59,35 @@ public class HomeFragment extends Fragment {
         route_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                HttpCall httpCallPost = new HttpCall();
+                httpCallPost.setMethodtype(HttpCall.POST);
+                httpCallPost.setUrl(UPDATE_SA_USER_URL);
+                HashMap<String,String> paramsPost = new HashMap<>();
+
+                for(int i = 0;i<adapter.selected_attractions.size();i++)
+                {
+                    paramsPost.put("selected_attractions"+"["+i+"]", String.valueOf((adapter.selected_attractions).get(i)));
+                }
+                paramsPost.put("userID", "9090");
+                httpCallPost.setParams(paramsPost);
+                new HttpRequest(){
+                    @Override
+                    public void onResponse(String response) {
+                        super.onResponse(response);
+                        String s = response;
+                        Log.d("UPDATE", s);
+                        try {
+                            Toast.makeText(getActivity().getApplicationContext(), response.toString(), Toast.LENGTH_SHORT).show();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+//                        if (s.equals("User added successfully")) {
+//                            startActivity(new Intent(SignUpActivity.this, MainActivity.class));
+//                        }
+                    }
+                }.execute(httpCallPost);
                 Log.d( "try",""+adapter.selected_attractions);
+                Log.d( "try",""+paramsPost);
             }
         });
 
